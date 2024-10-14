88a391e426da8438e5899ae42bdf5a5cdb1d469c
keisuke.mitsumune <keisuke.mitsumune@gmail.com>
2024-10-14 16:02:07 +0900

    tweak

--- src/lara9/app/Http/Controllers/ConversionController.php
+++ src/lara9/app/Http/Controllers/ConversionController.php
@@ -7,7 +7,20 @@ use App\Models\Conversion;
 class ConversionController extends \Illuminate\Routing\Controller
 {
     public function index() {
-        $data = Conversion::all();
-        echo $data->count();
+        $start = microtime(true);
+
+        $q = Conversion::query();
+        $recs = $q->lazyById(1000, $column = 'id');
+
+        $cnt = 0;
+        foreach ($recs as $rec) {
+            $cnt++;
+        }
+
+        echo implode('<br>', [
+            microtime(true) - $start,
+            memory_get_peak_usage(),
+            $cnt,
+        ]);
     }
 }
